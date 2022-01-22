c34ae6123c76c98ef83466c674130d45fd8e542e
Willem Boshoff <boshoffwillem@protonmail.com>
2022-01-22 22:19:19 +0200

    Use args iterator for rgrep

--- rgrep/src/main.rs
+++ rgrep/src/main.rs
@@ -4,7 +4,7 @@ use rgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -13,7 +13,7 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = rgrep::run(config) {
-        eprintln!("Application error: {}", e);
+        println!("Application error: {}", e);
         process::exit(1);
     }
 }
